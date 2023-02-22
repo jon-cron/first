92937adf27278a3753b61b8e59cb2c81b11e5d08
jon-cron <jonathanacm95@gmail.com>
2023-02-22 14:55:54 -0700

    push props, comps, and styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import "./Card.css";
 
 function Card(props) {
   // NOTE by adding props.className allows your wrapper to accept styling in other components
-  const classes = "card" + props.className;
+  const classes = "card " + props.className;
   // NOTE adding props.children allows you to use this comp as a wrap in other components
   return <div className={classes}>{props.children}</div>;
 }
