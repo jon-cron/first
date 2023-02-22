e3f53dea060c725deda8b7694e310708843207cb
jon-cron <jonathanacm95@gmail.com>
2023-02-22 13:25:29 -0700

    push comp styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
-
-
+// NOTE it is standard to save the css file name next to the comp and import the entire file as seen below
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
   return (
-    <>
-    <h2>Expense Item</h2>
-    <h2>Expense Item</h2>
-    </>
-  )
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
