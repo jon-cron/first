24e746bc36371bc2523119b8d66c12f2c60dc2a4
jon-cron <jonathanacm95@gmail.com>
2023-02-23 11:00:17 -0700

    passing state

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ExpenseForm from "./ExpenseForm.js";
 import "./NewExpense.css";
-const NewExpense = () => {
+const NewExpense = (props) => {
   const expenseDataHandler = (newExpenseData) => {
     const expenseData = {
       ...newExpenseData,
       id: Math.random().toString(),
     };
-    console.log(expenseData);
+    // console.log(expenseData);
+    props.onAddExpense(expenseData);
   };
   return (
     <div className="new-expense">
